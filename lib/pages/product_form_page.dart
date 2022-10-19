1cc85779201b6300662094657c0cab21dd34b785
Joao Felipe <jfcbxp@gmail.com>
2022-10-19 11:05:03 -0300

    adicionado formulario

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:shop/model/product.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
@@ -8,11 +11,121 @@ class ProductFormPage extends StatefulWidget {
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
+  final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _formData = Map<String, Object>();
+
+  @override
+  void dispose() {
+    _priceFocus.dispose();
+    _descriptionFocus.dispose();
+    _imageUrlFocus.dispose();
+//    _imageUrlFocus.removeListener(updateImage);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    //   _imageUrlFocus.addListener(updateImage);
+    super.initState();
+  }
+
+  void updateImage() {
+    setState(() {});
+  }
+
+  void _submitForm() {
+    _formKey.currentState?.save();
+    final newProduct = Product(
+        id: Random().nextDouble().toString(),
+        title: _formData['name'] as String,
+        description: _formData['description'] as String,
+        price: _formData['price'] as double,
+        imageUrl: _formData['imageUrl'] as String);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulario de Produto'),
+        actions: [IconButton(onPressed: _submitForm, icon: Icon(Icons.save))],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Nome'),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_priceFocus);
+                  },
+                  onSaved: (data) => _formData['name'] = data ?? '',
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Preço'),
+                  textInputAction: TextInputAction.next,
+                  focusNode: _priceFocus,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_descriptionFocus);
+                  },
+                  onSaved: (data) =>
+                      _formData['price'] = double.parse(data ?? '0'),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Descrição'),
+                  textInputAction: TextInputAction.next,
+                  focusNode: _descriptionFocus,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 3,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_imageUrlFocus);
+                  },
+                  onSaved: (data) => _formData['description'] = data ?? '',
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Url da Imagem'),
+                        textInputAction: TextInputAction.done,
+                        focusNode: _imageUrlFocus,
+                        keyboardType: TextInputType.url,
+                        controller: _imageUrlController,
+                        onFieldSubmitted: (_) => _submitForm(),
+                        onChanged: (value) => updateImage(),
+                        onSaved: (data) => _formData['imageUrl'] = data ?? '',
+                      ),
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      margin: EdgeInsets.only(
+                        top: 10,
+                        left: 10,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey, width: 1)),
+                      alignment: Alignment.center,
+                      child: _imageUrlController.text.isEmpty
+                          ? Text('Informe a Url')
+                          : FittedBox(
+                              child: Image.network(_imageUrlController.text),
+                              fit: BoxFit.cover,
+                            ),
+                    )
+                  ],
+                ),
+              ],
+            )),
       ),
     );
   }
